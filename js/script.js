6e98208d80bb79b71af0b4826cfe2935b7359a79
Parisha Bhagwatishanker <parishacb@gmail.com>
2023-05-31 17:15:32 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,83 @@
+// initialise the AOS library to add animations 
+AOS.init();
+
+// FUNCTION FOR PRELOADER STARTS 
+
+// FUNCTION FOR PRELOADER ENDS 
+
+// SCROLL TOP TOP BUTTON CODE STARTS
+let mybutton = document.getElementById("topBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+// SCROLL TOP TOP BUTTON CODE ENDS
+    
+// FUNCTION FOR TIMELINE - EXPERIENCE SECTION STARTS
+function qs(selector, all = false) {
+    return all ? document.querySelectorAll(selector) : document.querySelector(selector)
+}
+
+const sections = qs('.section', true);
+const timeline = qs('.timeline');
+const line = qs('.line');
+line.style.bottom = `calc(100% - 20px)`;
+let prevScrollY = window.scrollY;
+let up, down;
+let full = false;
+let set = 0;
+const targetY = window.innerHeight * 0.8;
+
+function scrollHandler(e){
+    const{
+        scrollY
+    } = window;
+    up = scrollY < prevScrollY;
+    down = !up;
+    const timelineRect = timeline.getBoundingClientRect();
+    const lineRect = line.getBoundingClientRect(); //CONST LINEHEIGHT = lineRect.bottom - lineRect.top
+
+    const dist = targetY - timelineRect.top
+    console.log(dist);
+
+    if (down && !full){
+        set = Math.max(set, dist);
+            line.style.bottom = `calc(100% - ${set}px)`
+    }
+
+    if (dist > timeline.offsetHeight + 50 && !full){
+        full = true;
+        line.style.bottom = `-50px`
+    }
+
+    sections.forEach(item => {
+        //console.log(items);
+        const rect = item.getBoundingClientRect();
+
+        if(rect.top + item.offsetHeight / 5 < targetY) {
+            item.classList.add('show-me')
+        }
+    });
+
+    prevScrollY = window.scrollY;
+}
+
+scrollHandler();
+line.style.display = 'block';
+window.addEventListener('scroll', scrollHandler)
+
+// FUNCTION FOR TIMELINE - EXPERIENCE SECTION ENDS
